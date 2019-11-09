449a4c896a07c5207c40ab278e7e7bd89714a142
maxdtd <max.schroen@gmail.com>
2019-11-09 17:59:47 +0100

    fixed compare.py string.translate error

--- compare.py
+++ compare.py
@@ -2,7 +2,6 @@
 from os import path
 import string
 import jiwer
-import sys
 
 # Jaccard Similarity of two strings
 def jaccard_similarity(doc1, doc2):
@@ -11,19 +10,22 @@ def jaccard_similarity(doc1, doc2):
     return len(intersection)/len(union)
 
 # Path declaration
-TRANSCRIPT_T2S_PATH = sys.argv[1]
-TRANSCRIPT_MANUAL = sys.argv[2]
+TRANSCRIPT_T2S_PATH = "../res/transcripts/auto_transcript/sam_harris.txt"
+TRANSCRIPT_MANUAL = "../res/transcripts/hw_transcript/sam_harris_tedsummit_can_we_build_transcript.txt"
+
+# Translation table for removing punctuation
+translation_table = str.maketrans("","",string.punctuation)
 
 # Read file contents and remove punctuation
 with open(TRANSCRIPT_T2S_PATH) as read_file:
     t2s_content = read_file.read()
 
-t2s_content = t2s_content.translate(None, string.punctuation)
+t2s_content = t2s_content.translate(translation_table)
 
 with open(TRANSCRIPT_MANUAL) as read_file:
     manual_content = read_file.read()
 
-manual_content = manual_content.translate(None, string.punctuation)
+manual_content = manual_content.translate(translation_table)
 
 # Compute Jaccard Similarity
 print("JACCARD SIMILARITY")
